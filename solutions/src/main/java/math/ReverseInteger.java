bc41c799df24f5693de3fbfdd02553271dd102d3
joewangyz <bladesofazzinoth@163.com>
2017-02-13 15:29:00 +0800

    create as maven project

---
+++ solutions/src/main/java/math/ReverseInteger.java
@@ -0,0 +1,23 @@
+package math;
+
+/**
+ * Created by wangyuzhou on 2017/2/13.
+ */
+public class ReverseInteger {
+    public int reverse(int x) {
+        int result = 0;
+        boolean flag = x < 0;
+        x = flag ? -x : x;
+        while (x > 0) {
+
+            int tmp = result * 10;
+            if (tmp / 10 != result) {
+                return 0;
+            }
+            result = tmp;
+            result += x % 10;
+            x /= 10;
+        }
+        return flag ? -result : result;
+    }
+}
