d8d3bc8b6f961b5452f3047986f7a9b0b6c0e17a
joewangyz <bladesofazzinoth@163.com>
2017-05-10 16:09:13 +0800

    update

--- solutions/src/main/java/common/CommonTree.java
+++ solutions/src/main/java/common/CommonTree.java
@@ -1,5 +1,9 @@
 package common;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * Created by joe wang on 2017/2/24.
  */
@@ -12,5 +16,81 @@ public class CommonTree {
         public TreeNode left;
         public TreeNode right;
         public TreeNode(int x) { val = x; }
+    }
+
+    public static class BTreeNode {
+        private int order;//
+        public int keyNum;
+        public BTreeNode parent;
+        public int[] key;
+        public BTreeNode[] childs;
+
+        public BTreeNode(int order) {
+            this.order = order;
+            this.key = new int[order + 1];
+            this.childs = new BTreeNode[order + 1];
+        }
+    }
+
+    TreeNode root;
+
+
+    public CommonTree(int[] array){
+        root=makeBinaryTreeByArray(array,1);
+    }
+
+
+
+    public static TreeNode makeBinaryTreeByArray(int[] array,int index){
+        if(index<array.length){
+            int value=array[index];
+            if(value!=0){
+                TreeNode t=new TreeNode(value);
+                array[index]=0;
+                t.left=makeBinaryTreeByArray(array,index*2);
+                t.right=makeBinaryTreeByArray(array,index*2+1);
+                return t;
+            }
+        }
+        return null;
+    }
+
+    public void levelOrderTravel() {
+        if(root == null){
+            System.out.println("empty tree");
+            return;
+        }
+        Queue<TreeNode> queue = new PriorityQueue<TreeNode>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode parent = queue.remove();
+            System.out.print(parent.val + " ");
+            if (null != parent.left) {
+                queue.add(parent.left);
+            }
+            if (null != parent.right) {
+                queue.add(parent.right);
+            }
+        }
+        System.out.print("\n");
+    }
+
+    public void depthOrderTravel() {
+        if (root == null) {
+            System.out.println("empty tree");
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            TreeNode parent = stack.pop();
+            System.out.print(parent.val + " ");
+            if (null != parent.right) {
+                stack.push(parent.right);
+            }
+            if (null != parent.left) {
+                stack.push(parent.left);
+            }
+        }
     }
 }
